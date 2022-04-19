cbcd72bf3f721d34d41e56d4b45f6132f09a8e23
RostislavGerasenko <91475179+RostislavGerasenko@users.noreply.github.com>
2022-04-19 03:41:10 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,2 +1,2 @@
 s = input("Enter the string: ")
-print(f"There are {sum(ord('a') <= ord(c) <= ord('z') for c in s)} lowercase Latin characters")
+print(f"There are {sum(ord('A') <= ord(c) <= ord('Z') for c in s)} uppercase Latin characters")
